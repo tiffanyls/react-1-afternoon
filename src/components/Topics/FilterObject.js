fdfa44015d0551a9dfc4b501cb4a6fac2ae892a9
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-01-26 16:11:57 -0600

    Steps 1 and 2 complete

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class FilterObject extends Component {
+    render () {
+        return (
+         <p> Filter Object </p>
+    )
+    }
+}
+
+export default FilterObject;
